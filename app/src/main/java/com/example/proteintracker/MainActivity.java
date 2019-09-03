c3d373eb2acd4b7f2e5321c1eb63dbcd9efe2615
jonathanalvin1998 <jonathan.alvin@si.ukdw.ac.id>
2019-09-03 19:26:19 +0700

    first commit

---
+++ app/src/main/java/com/example/proteintracker/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.proteintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener helpButtonListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this,HelpActivity.class);
+            Bundle b = new Bundle();
+            EditText myEditText = (EditText)findViewById(R.id.editText1);
+            b.putString("helpString",myEditText.getText().toString());
+            intent.putExtras(b);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.mainActivityTextView);
+        textView.setText(R.string.test_untuk_update_view);
+
+        Button helpBtn = (Button)findViewById(R.id.helpButton);
+        helpBtn.setOnClickListener(helpButtonListener);
+
+        Button myBtn = (Button)findViewById(R.id.button1);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText myEditText = (EditText)findViewById(R.id.editText1);
+                Log.d("ProteinTracker",myEditText.getText().toString());
+            }
+        });
+        if(savedInstanceState != null){
+            Log.d("ProteinTracker",savedInstanceState.getString("abc"));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString("abc","test");
+        super.onSaveInstanceState(outState);
+    }
+}
